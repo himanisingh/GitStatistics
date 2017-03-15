6fd13fd2c3e60179c6e4d4963fdba9825d3dcc4a
Himani Singh <HIMANIS@amdocs.com>
2017-03-15 19:59:15 +0530

    updating files #2

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var app = angular.module('gitStats', []);
+app.controller('GitStatsCtrl', ['$scope','$http', function($scope,$http) {
 
-app.controller('GitStatsCtrl', function($scope){
+$scope.getRepo = function(){
+	$http({
+	method: 'GET',
+	url: 'https://api.github.com/users/'+$scope.username+'/repos',
+	}).then(function successCallback(response) {    
+			$scope.repoData = response.data;
+	}, function errorCallback(response) {
+		console.log(response);
+	});
+};
 
-	$scope.getRepo = function(){
-		console.log($scope.username);
-	};
-
-});
+		
+}]);
 
 
